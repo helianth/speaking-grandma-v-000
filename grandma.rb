28770a068849b276703bc7bcb5e787797a308ffa
Lamia Haider <lamia.haider@gmail.com>
2018-04-03 00:24:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   phrase = gets.chomp
-  if phrase.upcase
+  if phrase.upcase == true
     puts "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else puts "HUH?! SPEAK UP, SONNY!"
   end
 end
-
-  
